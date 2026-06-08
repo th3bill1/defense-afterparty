b348cda90c11c37e5e9592d65f7bb00d6c632352
Wojciech Wójcik <wojtek2724@gmail.com>
2026-06-08 19:41:16 +0200

    v2.5

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,11 +1,49 @@
+import { useEffect, useRef, useState } from "react";
 import { Link, NavLink, Outlet } from "react-router";
 
 import { siteContent } from "../lib/site-content";
 
 export default function LayoutRoute() {
+  const [isHeaderHidden, setIsHeaderHidden] = useState(false);
+  const lastScrollYRef = useRef(0);
+  const tickingRef = useRef(false);
+
+  useEffect(() => {
+    lastScrollYRef.current = window.scrollY;
+
+    const updateHeaderVisibility = () => {
+      const currentScrollY = window.scrollY;
+      const scrollDelta = currentScrollY - lastScrollYRef.current;
+
+      if (currentScrollY < 24) {
+        setIsHeaderHidden(false);
+      } else if (scrollDelta > 6) {
+        setIsHeaderHidden(true);
+      } else if (scrollDelta < -6) {
+        setIsHeaderHidden(false);
+      }
+
+      lastScrollYRef.current = currentScrollY;
+      tickingRef.current = false;
+    };
+
+    const handleScroll = () => {
+      if (!tickingRef.current) {
+        tickingRef.current = true;
+        window.requestAnimationFrame(updateHeaderVisibility);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="shell">
-      <header className="site-header">
+      <header className={`site-header${isHeaderHidden ? " site-header--hidden" : ""}`}>
         <div className="container header-inner">
           <Link className="site-name" to="/">
             {siteContent.title}
